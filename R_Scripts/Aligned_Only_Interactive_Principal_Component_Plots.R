4e8919a0f84986d886ea4c7877fa7c1b401df5dc
James Mulqueeney <122978194+JamesMulqueeney@users.noreply.github.com>
2024-03-13 17:54:27 +0000

    Update Aligned_Only_Interactive_Principal_Component_Plots.R

--- R_Scripts/Aligned_Only_Interactive_Principal_Component_Plots.R
+++ R_Scripts/Aligned_Only_Interactive_Principal_Component_Plots.R
@@ -4,7 +4,7 @@
 
 # Author: James M. Mulqueeney 
 
-# Date Last Modified: 06/02/2024 
+# Date Last Modified: 13/03/2024
 
 # Aligned Only Comparison with Manual Landmarking Data 
 
@@ -19,7 +19,7 @@ library(cowplot)
 #########################################################################################
 
 # Read in colour/ shape of plot data
-colortable <- read.csv("E:/CTData/James Mulqueeney/Mammalian Data/Full Results/Coding Files/new_order_colors2.csv")
+colortable <- read.csv("path/to/input/new_order_colors2.csv")
 names(colortable$Order_colour)<-colortable$Order
 
 # Re-order the Orders (Needs working on)
@@ -30,7 +30,7 @@ phyloseq <- c("Zalambdalestidae", "Cimolesta", "Leptictida", "Cingulata", "Pilos
 #######################################
 
 # Read in the data 
-Kernel_40.0 <- read.csv("E:/CTData/James Mulqueeney/Papers/Write Up Papers/Paper 2- Comparison of Methods/Data/Results/Landmark Aligned/Kernel 40.0/kpca.csv")
+Kernel_40.0 <- read.csv("path/to/input/Kernel 40.0/kpca.csv")
 
 # Plot in 2D 
 
@@ -73,7 +73,7 @@ plot3
 #######################################
 
 # Read in the data 
-Kernel_20.0 <- read.csv("E:/CTData/James Mulqueeney/Papers/Write Up Papers/Paper 2- Comparison of Methods/Data/Results/Landmark Aligned/Kernel 20.0/kpca.csv")
+Kernel_20.0 <- read.csv("path/to/input/Kernel 20.0/kpca.csv")
 
 # Plot in 2D 
 
@@ -116,7 +116,7 @@ plot6
 #######################################
 
 # Read in the data 
-Kernel_10.0 <- read.csv("E:/CTData/James Mulqueeney/Papers/Write Up Papers/Paper 2- Comparison of Methods/Data/Results/Landmark Aligned/Kernel 10.0/kpca.csv")
+Kernel_10.0 <- read.csv("path/to/input/Kernel 10.0/kpca.csv")
 
 # Plot in 2D 
 
